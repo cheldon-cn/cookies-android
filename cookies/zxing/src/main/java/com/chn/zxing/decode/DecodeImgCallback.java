c39836f42f95c5f2233862eee7c5aef1bc79b3c6
cycle <ycae@outlook.com>
2022-02-11 14:09:37 +0800

    add: zxing module

---
+++ cookies/zxing/src/main/java/com/chn/zxing/decode/DecodeImgCallback.java
@@ -0,0 +1,15 @@
+package  com.chn.zxing.decode;
+
+import com.google.zxing.Result;
+
+/**
+ * Created by  cheldon on 2017/10/18.
+ *
+ * 解析图片的回调
+ */
+
+public interface DecodeImgCallback {
+    public void onImageDecodeSuccess(Result result);
+
+    public void onImageDecodeFailed();
+}
