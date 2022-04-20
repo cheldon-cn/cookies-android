e7b05c126681f41b2cbb97e3f7d000d146f52425
cheldon <program68@126.com>
2022-04-20 10:50:26 +0800

    add: weather module

---
+++ cookies/weather/src/main/java/com/chn/weather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.chn.weather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
